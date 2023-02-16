47d1f7899d51759c0b37ccdc9ec41dc69b19e031
RhysEvan <88195825+RhysEvan@users.noreply.github.com>
2023-02-16 12:24:31 +0100

    transformed original file to be a callable class with testing still possible

--- Inverse_Kinematics/main.py
+++ Inverse_Kinematics/main.py
@@ -8,33 +8,67 @@ from forward import *
 from static.presets_robot_models import preset_models
 import kinematics
 from inverse import with_torch
+class motor_inverter():
+    def __init__(self, coordinates = None):
+        self.alpha, self.theta, self.radius, self.dists, self.active, self.limits = get_DH_params(preset_models["HangingArm"])
+        self.For_model = kinematics.Kinematics("HangingArm")
+        self.model = torch.load(r".\kine_models\HangingArm.pt")
+        if coordinates == None:
+            self.gen_test()
+        else:
+            self.transform(coordinates)
+    
+    def transform(self, coord):
+        n = len(coord)
+        self.motor_positions = self.process(n, coord)
+    
+    def process(self, n, inputs):
+        #######################
+        pred = self.model(torch.Tensor(inputs[n:n+1])).detach()
+        ######################
+        self.For_model.set_active(pred[0])
+        self.For_model.update_position()
+        pos_pred = self.For_model.run_forward().round(2)
+        ######################
+        print("--- x y z ---")
+        print(inputs[n].round(2))
+        print(pos_pred)
+        ######################
+        print("--- torch ---")
+        pred_xys = with_torch().forward_from_active(self.For_model, pred)
+        print(pred_xys[0,0].numpy().round(2))
+        print("--- motor positions ---")
+        print(pred.numpy().round(2)[0])
+        return pred
 
-alpha, theta, radius, dists, active, limits = get_DH_params(preset_models["HangingArm"])
-For_model = kinematics.Kinematics("HangingArm")
-model = torch.load(r".\kine_models\HangingArm.pt")
 
-inputs, target = For_model.generate_maps(n=1)
-#mot_pos = np.array([alpha,theta,radius,dists])
-n= np.random.randint(len(inputs))
-#######################
-pred = model(torch.Tensor(inputs[n:n+1])).detach()
-######################
-For_model.set_active(pred[0])
-For_model.update_position()
-pos_pred = For_model.run_forward().round(2)
-#######################
-For_model.set_active(target[n])
-pos_targ = For_model.run_forward().round(2)
-########################
-print("--- x y z ---")
-print(inputs[n].round(2))
-print(pos_targ)
-print(pos_pred )
-print("--- torch ---")
-pred_xys = with_torch().forward_from_active(For_model, target[n:n+1])
-print(pred_xys[0,0].numpy().round(2))
-pred_xys = with_torch().forward_from_active(For_model, pred)
-print(pred_xys[0,0].numpy().round(2))
-print("--- motor positions ---")
-print(target[n].round(2))
-print(pred.numpy().round(2)[0])
+    def gen_test(self):
+        inputs, target = self.For_model.generate_maps(n=1)
+        #mot_pos = np.array([alpha,theta,radius,dists])
+        n= np.random.randint(len(inputs))
+        #######################
+        pred = self.model(torch.Tensor(inputs[n:n+1])).detach()
+        ######################
+        self.For_model.set_active(pred[0])
+        self.For_model.update_position()
+        pos_pred = self.For_model.run_forward().round(2)
+        #######################
+        self.For_model.set_active(target[n])
+        pos_targ = self.For_model.run_forward().round(2)
+        ########################
+        print("--- x y z ---")
+        print(inputs[n].round(2))
+        print(pos_targ)
+        print(pos_pred )
+        print("--- torch ---")
+        pred_xys = with_torch().forward_from_active(self.For_model, target[n:n+1])
+        print(pred_xys[0,0].numpy().round(2))
+        pred_xys = with_torch().forward_from_active(self.For_model, pred)
+        print(pred_xys[0,0].numpy().round(2))
+        print("--- motor positions ---")
+        print(target[n].round(2))
+        print(pred.numpy().round(2)[0])
+
+
+if __name__ == "__main__":
+    motor_inverter()
