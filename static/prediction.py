f1ecf1622ce351b0c00586a3cef3270a4e4d6f35
RhysEvan <88195825+RhysEvan@users.noreply.github.com>
2023-03-22 22:06:09 +0100

    prediction transformed to thinning instead of skeletonize

--- static/prediction.py
+++ static/prediction.py
@@ -1,6 +1,6 @@
 import Retna_run_training
 import numpy as np
-from skimage.morphology import skeletonize
+from skimage.morphology import skeletonize, thin
 from skimage.transform import resize
 import cv2
 from PyQt5.QtWidgets import *
@@ -20,11 +20,11 @@ class prediction():
             #crop = self.cropper(image, contours)
             pred = self.retna.cam_predict(image, r"C:\Users\mheva\OneDrive\Documents\GitHub\Stitching_Arm_Master_Thesis\Retna\models", "\\pleora_state_dict_2.pt")
             #skeleton = pred
-            skeleton = skeletonize(pred)
+            skeleton = thin(pred, max_num_iter = 5)
             image = (resize(image, (480,640))*255).mean(axis=2)
-            image = np.tile(image[...,None],[1,1,3]).astype(np.uint8)
+            #image = np.tile(image[...,None],[1,1,3]).astype(np.uint8)
             skeleton =  (resize(skeleton, image.shape)*255).astype(np.uint8)
-            image[skeleton>100] = skeleton[skeleton >100]
+            image[skeleton>10] = skeleton[skeleton >10]
             return image
 
     def cropper(self,im,contours):
